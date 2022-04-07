0bbe248ff40b0b9e0d8ecbd5ceed4c582cca8e42
AndreaMaestri18 <andreamaestrimaestri@gmail.com>
2022-04-07 10:44:43 +0200

    passaggio consegne

--- index.py
+++ index.py
@@ -1,7 +1,30 @@
 from pydicom import dcmread
 from pydicom.data import get_testdata_file
+import matplotlib.pyplot as plt
+import pydicom
+from pydicom.data import get_testdata_files
 
 fpath = get_testdata_file("CT_small.dcm")
 ds = dcmread(fpath)
 
+# dal path del file converto in dcm 
+fpath2 = 'out/1.3.6.1.4.1.5962.1.1.1.1.1.20040119072730.12322'
+with open(fpath2, 'rb') as infile:
+
+    ds = dcmread(infile)
+
+
+plt.imshow(ds.pixel_array, cmap=plt.cm.bone) 
+plt.savefig('collo.jpeg')
+plt.show()
+
+    
 print(ds)
+print(ds.SOPClassUID)
+print(ds.ImageType)
+print(ds.PatientID)
+print(ds.PatientSex)
+print(ds.pixel_array)
+
+
+
--- scp.py
+++ scp.py
@@ -10,37 +10,41 @@ debug_logger()
 
 def handle_store(event, storage_dir):
     """Handle EVT_C_STORE events."""
+    # Crea cartella o tira errore 
     try:
         os.makedirs(storage_dir, exist_ok=True)
     except:
         # Unable to create output dir, return failure status
         return 0xC001
 
+    # join del path della cartella con il nome del dataset 
     fname = os.path.join(storage_dir, event.request.AffectedSOPInstanceUID)
+
     with open(fname, 'wb') as f:
         # Write the preamble, prefix and file meta information elements
+        # TODO: Capire bene queste cose
         f.write(b'\x00' * 128)   
         f.write(b'DICM')
         # data_set = dcmread(f)
         write_file_meta_info(f, event.file_meta)
         # Write the raw encoded dataset
-        #mycode start
+        # mycode start
         # date = data_set[0x0008,0x0012]
-        # user = [0x0010,0x0010]
+        # user = data_set[0x0010,0x0010]
         # print(date, user)
-        #mycode end
+        # mycode end
         f.write(event.request.DataSet.getvalue())
    
 
     return 0x0000
 
+# handlers degli eventi scatenati
 handlers = [(evt.EVT_C_STORE, handle_store, ['out'])]
 
+# instanza dell'application entity, entità coinvolta nella comunicazione dicom
 ae = AE()
 
-storage_sop_classes = [
-    cx.abstract_syntax for cx in AllStoragePresentationContexts
- ]
+storage_sop_classes = [cx.abstract_syntax for cx in AllStoragePresentationContexts]
 for uid in storage_sop_classes:
     ae.add_supported_context(uid, ALL_TRANSFER_SYNTAXES)
 
